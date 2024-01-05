3cc5ea689f80052b1451d5091a794dffada296b6
Sergi <sergi20031005@gmail.com>
2024-01-05 16:42:09 +0100

    Add lazy-image component and update sidebar component

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home/home.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { CommonModule } from '@angular/common';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { HomePageComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
-  imports: [CommonModule],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent,
+    GifsCardComponent,
+  ],
+  imports: [CommonModule, SharedModule],
   exports: [HomePageComponent],
 })
 export class GifsModule {}
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 import { GifsService } from '../../../gifs/services/gifs.service';
 import { GifsModule } from '../../../gifs/gifs.module';
 
@@ -8,9 +8,14 @@ import { GifsModule } from '../../../gifs/gifs.module';
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  public tagInput!: ElementRef<HTMLButtonElement>;
   constructor(private gifsService: GifsService) {}
 
-  get tags() {
+  get tags(): string[] {
     return this.gifsService.tagsHistory;
+  }
+
+  searchTag(tag: string) {
+    this.gifsService.searchTag(tag);
   }
 }
